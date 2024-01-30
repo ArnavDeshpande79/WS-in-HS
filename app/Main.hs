3c4022ef3836d1ad787fab11ec05c23de30edd8e
Arnav Deshpande <arnavdeshpande@Arnavs-MacBook-Pro.local>
2024-01-29 20:56:10 -0800

    Added lexical token generation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
+import Types
+import System.IO
+import System.Directory
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main :: IO (Int)
+main = do
+	input <- readFile "./data/whitespace.txt"
+	let tokens = filter (/=Types.Invalid) . (map Types.parse) $ input
+	--let parseTree = makeParseTree tokens
+	in putStrLn (show tokens)
+	return 0
